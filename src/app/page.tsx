65f4b236e1487978f55b67c713299c347f6a9e6a
LAPTOP-9LEO7DAD\zxmacoi <zxmac.dev@gmail.com>
2025-09-08 10:14:20 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import ProSummary from "./components/pro-summary";
 import TechSkills from "./components/tech-skills";
 import { CommonLib } from "./lib/common.lib";
 import { Cv } from "./models/cv.model";
+import Spinner from "./components/ui/spinner/spinner";
 import { CvService } from "./services/cv.service";
 
 export default function Home() {
   const [cv, setCv] = useState<Cv>({ profile: { name: '', position: '' } } as Cv);
+  const [showLoader, setShowLoader] = useState(true);
   
   useEffect(() => {
     const sp =  CommonLib.getSearchParams(window);
@@ -18,16 +20,22 @@ export default function Home() {
     (async () => {
       const data = await CvService.get(sp.s, sp.a);
       setCv(data);
+      setShowLoader(false);
     })();
   }, []);
   
   return (
     <>
-      <Header data={cv.profile} />
-      <ProSummary data={cv.summary} />
-      <TechSkills data={cv.techStack} />
-      <ProExperience data={cv.experience} />
-      <Education data={cv.education} />
+      { !showLoader ?
+        <>
+          <Header data={cv.profile} />
+          <ProSummary data={cv.summary} />
+          <TechSkills data={cv.techStack} />
+          <ProExperience data={cv.experience} />
+          <Education data={cv.education} />
+        </>
+        : <Spinner />
+      }
     </>
   );
 }
